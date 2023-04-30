c22dad24bc0afd21008ab6e65ce1690ae0bb4217
Grendaizo90 <konstkam@mail.ru>
2023-04-30 22:15:17 +0300

    Product component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, HeaderComponent, ProductComponent],
     imports: [BrowserModule],
     providers: [],
     bootstrap: [AppComponent],
