cc32d252bc67a334fa9d1e56b0cbe9dde61dcf6b
Grendaizo90 <konstkam@mail.ru>
2023-05-01 14:50:46 +0300

    Fake error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ProductComponent } from './components/product/product.component';
+import { GlobalErrorComponent } from './components/global-error/global-error.component';
 
 @NgModule({
-    declarations: [AppComponent, HeaderComponent, ProductComponent],
+    declarations: [AppComponent, HeaderComponent, ProductComponent, GlobalErrorComponent],
     imports: [BrowserModule, HttpClientModule],
     providers: [],
     bootstrap: [AppComponent],
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,21 +1,29 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+import { Observable, catchError, delay, retry, throwError } from 'rxjs';
 import { Product } from '../models/product';
+import { ErrorService } from './error.service';
 
 @Injectable({
     providedIn: 'root',
 })
 export class ProductService {
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private errorService: ErrorService) {}
 
     getAll(): Observable<Product[]> {
-        return this.http.get<Product[]>('https://fakestoreapi.com/products', {
-            params: new HttpParams({
-                fromObject: {
-                    limit: 5,
-                },
-            }),
-        });
+        return this.http
+            .get<Product[]>('https://fakestoreapi.com/products1', {
+                params: new HttpParams({
+                    fromObject: {
+                        limit: 5,
+                    },
+                }),
+            })
+            .pipe(delay(1000), retry(2), catchError(this.errorHandler.bind(this)));
+    }
+
+    private errorHandler(error: HttpErrorResponse) {
+        this.errorService.handle(error.message);
+        return throwError(() => error.message);
     }
 }
